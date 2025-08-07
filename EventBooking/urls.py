3d5a3e6a46b56555a1bf5884eeb12a4482f189ba
qwertyllionman <150654251+qwertyllionman@users.noreply.github.com>
2025-08-07 13:31:11 +0500

    Update urls.py
    token verify API endpoint is removed.

--- EventBooking/urls.py
+++ EventBooking/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', include('apps.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 
